f355d40ef9474aa286846929912c4631fad037eb
Sandeep <stengali33@gmail.com>
2017-04-08 12:18:20 -0700

    Creating end points

--- app.py
+++ app.py
@@ -6,4 +6,16 @@ app = Flask(__name__)
 def home():
     return "Hello, world!"
 
+@app.route('/') # 'http://www.tojafit.com/'
+def home():
+    return "Hello, world!"
+
+@app.route('/checkTeamName/<string:teamName>')
+def checkTeamName(teamName):
+    pass
+
+@app.route('/createTeam', methods=['POST'])
+def createTeam():
+    pass
+
 app.run(port=5000)

58825b95fedd3cbd89490c3f635d09763ed3ae37
Sandeep <stengali33@gmail.com>
2017-04-08 12:24:39 -0700

    new end points

--- app.py
+++ app.py
@@ -2,20 +2,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/') # 'http://www.tojafit.com/'
-def home():
-    return "Hello, world!"
 
 @app.route('/') # 'http://www.tojafit.com/'
 def home():
     return "Hello, world!"
 
+#GET method to check if the team name is already taken.
 @app.route('/checkTeamName/<string:teamName>')
 def checkTeamName(teamName):
     pass
 
+#POST method to create new team. This method requires unique teamName.
 @app.route('/createTeam', methods=['POST'])
-def createTeam():
+def create_team():
+    pass
+
+@app.route('/getTeamList/<string:orgName')
+def get_team_list(orgName):
+    pass
+
+@app.route('/joinTeam',methods=['POST'])
+def join_team():
     pass
 
 app.run(port=5000)

74185baf6de3a21aebce5eb3c5c2f65fdc555be4
Sandeep <stengali33@gmail.com>
2017-04-08 12:36:01 -0700

    Sample rest call

--- app.py
+++ app.py
@@ -1,25 +1,48 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/') # 'http://www.tojafit.com/'
+teams = [
+    {
+        'name' : 'team1',
+        'members' : [
+            {
+                'email' : 'stengali'
+            },
+            {
+                'email' : 'aggarwal'
+            }
+        ]
+    },
+    {
+        'name' : 'team2'
+    }
+]
+
+
+
+@app.route('/')  # 'http://www.tojafit.com/'
 def home():
     return "Hello, world!"
 
-#GET method to check if the team name is already taken.
+
+# GET method to check if the team name is already taken.
 @app.route('/checkTeamName/<string:teamName>')
-def checkTeamName(teamName):
+def check_team_name(teamName):
     pass
 
-#POST method to create new team. This method requires unique teamName.
+
+# POST method to create new team. This method requires unique teamName.
 @app.route('/createTeam', methods=['POST'])
 def create_team():
     pass
 
-@app.route('/getTeamList/<string:orgName')
-def get_team_list(orgName):
-    pass
+
+@app.route('/getTeamList')
+def get_team_list():
+    return jsonify({'teams':teams})
+
 
 @app.route('/joinTeam',methods=['POST'])
 def join_team():

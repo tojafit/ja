6b65f673cc5fbd5ec5230d5d75324592dd39cbb1
Sandeep <stengali33@gmail.com>
2017-04-09 15:30:14 -0700

    Join and Create team

--- code/app.py
+++ code/app.py
@@ -1,14 +1,58 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
+organizations = {
+    'teams' : [
+        {
+            'name' : 'team1',
+            'members' : [
+                {
+                    'email' : 'stengali'
+                },
+                {
+                    'email' : 'aggarwal'
+                }
+            ]
+        },
+        {
+            'name' : 'team2'
+        }
+    ]
+}
 
-class Student(Resource):
-    def get(self, name):
-        return {'student': name}
 
-api.add_resource(Student, '/student/<string:name>')
+class CreateTeam(Resource):
+    def post(self):
+        request_data = request.get_json()
+        team_name = request_data['team_name']
+        member_email = request_data['member_email']
+        for team in organizations['teams']:
+            if team['name'] == team_name:
+                return {'status': "Team name already present"}, 404
+        team = {'name': team_name , 'members': [{'email':member_email}]}
+        organizations['teams'].append(team)
+        return 200
+
+
+class JoinTeam(Resource):
+    def post(self):
+        request_data = request.get_json()
+        team_name = request_data['team_name']
+        member_email = request_data['member_email']
+        for team in organizations['teams']:
+            if team['name'] == team_name:
+                team['members'].append(member_email)
+                return 200
+        return {'status': "Invalid team name"}, 404
+
+
+api.add_resource(CreateTeam, '/create_team')
+api.add_resource(JoinTeam, '/join_team')
 
 app.run(port=5000)
+
+
+
